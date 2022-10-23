9c5dc082c5fff2a0bc2528a9ea2e4a4bef1a723a
kartiksoni1002 <106035727+kartiksoni1002@users.noreply.github.com>
2022-10-23 05:19:32 -0400

    Assignment 2

---
+++ src/pages/about.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="main">
+      <div className="part1">
+        <div className="part2">
+         
+        </div>
+      </div>
+
+      <div class="About">
+        <text>
+          <h2><b>Information</b></h2>
+          <h2>Personal Info.</h2> <hr></hr>Hello there, my name is Kartik Soni. I am 23 years old. I have completed my bachelors in computer engineering. Currently i am learning mobile application developement course i Georgian College. 
+        </text>
+        <text>
+          <h2>Skills</h2>
+          <hr></hr>
+          1 - Hardworking
+          2 - Consistent
+          3 - cooperative
+        </text>
+      </div>
+    </div>
+  );
+};
+
+export default About;
